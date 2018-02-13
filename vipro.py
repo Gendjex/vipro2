cbb6ba5d8dab4acea9ff49b37f37735e75380bf3
Gendjex <33978766+Gendjex@users.noreply.github.com>
2018-02-13 16:11:39 +0000

    Update vipro.py

--- vipro.py
+++ vipro.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#Chucky_Bot
+#Vipro_Bot
 
 import LINETCR
 from LINETCR.lib.curve.ttypes import *
@@ -12,7 +12,7 @@ import time,random,sys,json,codecs,threading,glob,urllib,urllib2,urllib3,re,ast,
 
 vipro = LINETCR.LINE()
 #vipro.login(qr=True)
-vipro.login(token='EpJgyNvzK8xQp88I04t9.2PnOnQMgNRHUYkUzkK6jEq./p92mOc1BPmafZPSgTcw4mkpBeT/czsWHBLRbK7Aewc=')
+vipro.login(token='')
 vipro.loginResult()
 print "Vipro-Login Success\n\n=====[Sukses Login]====="
 
